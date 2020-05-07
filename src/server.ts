10a1f802ece4714d3f570da3babd67b7037f8f07
Noisycall <45820995+Noisycall@users.noreply.github.com>
2020-05-07 12:25:18 +0530

    Added React Frontend

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
-
+import cors from "cors"
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 import items from "./routes/api/items";
+import path from "path";
 
 const db = require("./keys").mongoURI;
 mongoose
@@ -16,7 +18,12 @@ mongoose
       console.error(err);
     });
 
-app.get("/",(req,res)=>{res.json({k:"wow"})})
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.use("/api/items",items);
 const port = process.env.PORT || 5000;
+
+app.get("*",(req,res)=>{
+  res.sendFile(path.resolve(__dirname,'../client/build/index.html'))
+})
 app.listen(port,()=>{console.log(`Server Started on port ${port}`)});
+
